3345c3b51f51d8b81324d2edffb3ae152e60aea8
vmargaba <113616421+vmargaba@users.noreply.github.com>
2024-06-13 13:26:17 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -7,14 +7,14 @@ const util = require('util')
 const readDir = util.promisify(fs.readdir) 
 const rename = util.promisify(fs.rename) 
 
-const renameOperation = async (oldpath, newpath) => { 
+const renameOperation = (oldpath, newpath) => { 
 console.log(`Contents before rename operation: `) 
 	
 // Fetching contents of the directory before 
 // rename operation. The process.cwd() gives 
 // current working directory 
 const oldFiles = await 
-	fs.promises.readdir(process.cwd()) 
+	fs.promises.readDirectory(process.cwd()) 
 
 // Contents of the current working directory 
 for(let file of oldFiles){ 
@@ -24,13 +24,13 @@ for(let file of oldFiles){
 console.log('\nAttempt to rename file : \n') 
 
 // Rename operation 
-await fs.promises.rename(oldpath, newpath) 
+await fs.promises.renameFile(oldpath, newpath) 
 console.log(`Contents after rename operation: `) 
 	
 // Fetching contents of directory before 
 // rename operation 
 const newFiles = await 
-	fs.promises.readdir(process.cwd()) 
+	fs.promises.readDirectory(process.cwd()) 
 	
 // Contents of the current working directory 
 for(let file of newFiles) { 
@@ -44,3 +44,5 @@ console.log(`Error occurs, Error code -> ${err.code},
 Error No -> ${err.errno}`); 
 }); 
 
+// Introducing an undefined variable error
+console.log(undeclaredVariable);
